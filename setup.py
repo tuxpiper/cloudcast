4f099ad0c37dbaeeeb6f069901d661095c7fda84
David Losada <david@tuxpiper.com>
2015-08-17 00:45:00 +0200

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "cloudcast",
-    version = "0.1.0",
+    version = "0.1.1",
     packages = find_packages(),
     package_data = {
         # Script files that contain initial bootstrap sequences
